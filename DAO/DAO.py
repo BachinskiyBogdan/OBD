70c543a2b470d2ee6938797be083ce176d8dbca6
max <maxkasich@gmail.com>
2012-10-06 13:42:20 -0700

    Intefaces
    I hate git hub. I lost 2 point of my mark because it not commited this
    files at last week.

---
+++ DAO/DAO.py
@@ -0,0 +1,17 @@
+class DBManager(object):
+    def __init__(self, file_name, file_open_mode):
+        pass
+
+    def get_string(self, index):
+        pass
+
+    def set_string(self, index, string):
+        pass
+
+    def to_entity(self, string):
+        pass
+
+    def to_string(self, entity):
+        pass
+
+    #def Метод, который вызывается при использовании оператора with
---
+++ Repository/ProductRepository.py
@@ -0,0 +1,15 @@
+class ProductRepository(object):
+    def __init__(self):
+        pass
+
+    def read(self, start_index, number_of_entities):
+        pass
+
+    def add(self, new_entity):
+        pass
+
+    def update(self, entity):
+        pass
+
+    def delete(self, entity):
+        pass
